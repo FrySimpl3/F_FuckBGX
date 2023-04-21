602a9bba69ac575792f67244a5fd59f442b75012
Fry <77803482+FrySimpl3@users.noreply.github.com>
2023-04-21 18:44:04 +0700

    Add project files.

---
+++ ConsoleApp1/Fry.cs
@@ -0,0 +1,98 @@
+﻿
+
+namespace ConsoleApp1
+{
+    internal static class Func
+    {
+        public static bool checkPer()
+        {
+            System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
+            bool isAdmin = principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+            if (isAdmin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void _printf(string msg="",int sleep=0,System.ConsoleColor consoleColor = System.ConsoleColor.White)
+        {
+            Console.ForegroundColor = consoleColor;
+            string _out = DateTime.Now.ToString("[ HH:mm:ss dd/MM/yyyy ]\t") + msg;
+            foreach (var item in _out)
+            {
+                Console.Write(item);
+                System.Threading.Thread.Sleep(sleep);
+            }
+            Console.ForegroundColor = System.ConsoleColor.White;
+            Console.WriteLine();
+        }
+        public static void _mySign()
+        {
+            string _out = DateTime.Now.ToString("[ HH:mm:ss dd/MM/yyyy ]      ");
+            string txt = $"{_out}Code By Fry\n{_out}Discord : Frysimpl3#7958";
+            System.IO.File.AppendAllText("Fry.ini", txt);
+        }
+        public static string FindAppDataFolder()
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string[] directories = Directory.GetDirectories(appDataPath);
+            foreach (string dir in directories)
+            {
+                string path = Path.GetRelativePath(appDataPath, dir);
+                if (path.Length == 32)
+                {
+                    if (System.IO.File.Exists(dir+"\\config.bin") && System.IO.File.Exists(dir + "\\session.dat"))
+                    {
+                        return dir;
+                    }
+                    else
+                    {
+                        ConsoleApp1.Func._printf("Press any key exit", 1, ConsoleColor.DarkMagenta);
+                        Console.ReadKey();
+                        System.Environment.Exit(0);
+                    }
+                }
+            }
+            return null;
+        }
+        public static string getUrl(string url)
+        {
+            string result = string.Empty;
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                result = client.DownloadString(url);
+            }
+            Uri uriResult;
+            if (Uri.TryCreate(result, UriKind.Absolute, out uriResult) &&(uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+            {
+                _printf("Url hợp lệ");
+                return uriResult.ToString();
+            }
+            else
+            {
+                Console.WriteLine("Chuỗi không phải là URL hợp lệ.");
+                return null;
+            }
+        }
+        public static bool dowlaodFile(string url, string localPath) 
+        {
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                client.DownloadFile(url, localPath);
+            }
+            System.Threading.Thread.Sleep(1000);
+            if (File.Exists(localPath))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Fry
+{
+    class _main
+    {
+        static void Main(string[] args)
+        {
+            //System.Diagnostics.Process.Start("https://bothax.com/");
+            bool check = ConsoleApp1.Func.checkPer();
+            if (!check) {
+                ConsoleApp1.Func._printf("Pls run me with admin!", 1, ConsoleColor.DarkRed);
+                ConsoleApp1.Func._printf("Press any key exit", 1, ConsoleColor.DarkMagenta);
+                Console.ReadKey();
+                return;
+            }
+            ConsoleApp1.Func._mySign();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            ConsoleApp1.Func._printf("Dev by FrySimpl3",1,ConsoleColor.DarkRed);
+            string path = ConsoleApp1.Func.FindAppDataFolder();
+            Console.WriteLine(path +"fry.ini");
+            ConsoleApp1.Func._printf(path == string.Empty ? "Không Tìm Thấy Thư Mục":"Đã Tìm Thấy Thư Mục : "+ path, 1, ConsoleColor.DarkGreen);
+            if (path ==null)
+            {
+                ConsoleApp1.Func._printf("Press any key exit", 1, ConsoleColor.DarkMagenta);
+                Console.ReadKey();
+                return;
+            }
+            string urlOfConfig = "https://api.pastecode.io/anon/raw-snippet/i2t2zxh7?raw=inline&ticket=5fdd97f7-7a1c-42bf-99b9-54a2d8551582";
+            Uri uriResult;
+            string Url = string.Empty;
+            if (Uri.TryCreate(urlOfConfig, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+            {
+                Url = ConsoleApp1.Func.getUrl(urlOfConfig);
+                System.Threading.Thread.Sleep(1000);
+                ConsoleApp1.Func.dowlaodFile(Url, "Fry.Config");
+                File.Copy("Fry.Config", path + "\\config.bin", true);
+            }
+            Console.ReadKey();
+        }
+    }
+}
